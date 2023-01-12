64edb6f8404c9bad730dc25ec4bb03f1e033d467
sabbirhossain <smsabbir925@gmail.com>
2023-01-11 21:52:31 -0800

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,28 @@
+import Hero from "./Hero";
+import GetStarted from "./GetStarted";
+import Footer from "./Footer";
+import CTA from "./CTA";
+import Client from "./Client";
+import CartDeal from "./CartDeal";
+import Button from "./Button";
+import Business from "./Business";
+import Billing from "./Billing";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import Navbar from "./Navbar";
+import FeedbackCard from "./FeedbackCard";
+export {
+  Testimonials,
+  Stats,
+  Billing,
+  Business,
+  Button,
+  CartDeal,
+  Client,
+  CTA,
+  Footer,
+  GetStarted,
+  Hero,
+  Navbar,
+  FeedbackCard,
+};
